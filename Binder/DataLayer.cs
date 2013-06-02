49160234f6db66810d3b56ce69be8d4e97eb2734
pcgalen <goreville1>
2013-06-02 05:53:42 -0500

    Enhance DataLayer

--- Binder/DataLayer.cs
+++ Binder/DataLayer.cs
@@ -1,4 +1,6 @@
-﻿using System.Data.SqlClient;
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Binder
 {
@@ -7,13 +9,43 @@ namespace Binder
 
         private static SqlConnection m_oCn;
         private static bool m_bInitialized = false;
+        private static long m_lErrorNo = 0;
+        private static string m_sErrorMsg = "";
 
+        public bool Initialized
+        {
+            get
+            {
+                return m_bInitialized;
+            }
+        }
+        public long ErrorNo
+        {
+            get
+            {
+                return m_lErrorNo;
+            }
+        }
+
+        public string Errormsg
+        {
+            get
+            {
+                return m_sErrorMsg;
+            }
+        }
         public SqlConnection Connection
         {
             get
             {
                 return m_oCn;
             }
+        }
+
+        private void ResetError()
+        {
+            m_lErrorNo = 0;
+            m_sErrorMsg = "";
         }
 
         public static void DataLayer()
@@ -26,10 +58,66 @@ namespace Binder
             }
             catch (SqlException ex)
             {
-
+                m_lErrorNo = ex.ErrorCode;
+                m_sErrorMsg = ex.Message;
+                m_bInitialized = false;
+                return;
             }
+
+            m_bInitialized = true;
         }
 
+        public void ExecSQL(string pSql, List<SqlParameter> oParams)
+        {
+            ResetError();
+            SqlCommand oCmd = new SqlCommand(pSql, m_oCn);
+
+            foreach (SqlParameter oParam in oParams)
+            {
+                oCmd.Parameters.Add(oParam);
+            }
+
+            try
+            {
+                oCmd.ExecuteNonQuery();
+            }
+            catch (SqlException ex)
+            {
+                m_lErrorNo = ex.ErrorCode;
+                m_sErrorMsg = ex.Message;
+            }
+
+        }
+
+        public DataTable ExecSql(string pSql, List<SqlParameter> pParams)
+        {
+            ResetError();
+
+            DataTable oDt = new DataTable();
+            SqlCommand oCmd = new SqlCommand(pSql, m_oCn);
+            SqlDataAdapter oAdapter = new SqlDataAdapter();
+
+            foreach (SqlParameter oParam in pParams)
+            {
+                oCmd.Parameters.Add(oParam);
+            }
+
+            oAdapter.SelectCommand = oCmd;
+
+            try
+            {
+
+                oAdapter.Fill(oDt);
+            }
+            catch (SqlException ex)
+            {
+                m_lErrorNo = ex.ErrorCode;
+                m_sErrorMsg = ex.Message;
+                return null;
+            }
+
+            return oDt;
+        }
 
     }
 }
