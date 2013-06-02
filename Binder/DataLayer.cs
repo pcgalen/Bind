db900190de56fcbf20cb656643d448881dcbc6a0
pcgalen <galen@hendersonsystems.co>
2013-06-02 04:49:24 -0500

    Add DataLayer

---
+++ Binder/DataLayer.cs
@@ -0,0 +1,35 @@
+﻿using System.Data.SqlClient;
+
+namespace Binder
+{
+    public class DataLayer
+    {
+
+        private static SqlConnection m_oCn;
+        private static bool m_bInitialized = false;
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return m_oCn;
+            }
+        }
+
+        public static void DataLayer()
+        {
+            m_oCn = new SqlConnection(Util.getConn());
+
+            try
+            {
+                m_oCn.Open();
+            }
+            catch (SqlException ex)
+            {
+
+            }
+        }
+
+
+    }
+}
